6f701bca28a97d0f3bf761e38aa46be41e75545a
zimkus <mutka01@gmail.com>
2024-02-13 22:10:29 +0200

    Java mini uzduoteles update

---
+++ java-mini-uzduoteles/uzduotys.js
@@ -0,0 +1,55 @@
+// KINTAMUJU INICIJAVIMAS
+//1.
+
+const x1 = 4;
+const x2 = -5;
+const x3 = NaN;
+
+console.log(x1);
+console.log(x2);
+console.log(x3);
+
+console.clear();
+
+//2.
+
+const y1 = 'labas mano';
+const y2 = 'vardas';
+const y3 = 'Eimantas';
+
+console.log(y1);
+console.log(y2);
+console.log(y3);
+
+console.clear();
+
+//3.
+
+const z1 = [2, 4, 2.5, -24, Infinity]
+const z2 = [-1, -2, -3, -4, NaN]
+const z3 = [1, 2, 3, 2, 2]
+console.log(z1,);
+console.log(z2);
+console.log(z3);
+
+console.clear();
+
+//4.
+
+const p1 = ['a', 'b', 'c', 'd', 'e']
+for (let i = 0; i < 5; i++) {
+    console.log(p1[i]);
+}
+const p2 = ['labas', 'as', 'esu', 'Chuck', 'Norris'];
+console.log(p2[0], p2[1], p2[2], p2[3], p2[4]);
+const p3 = [':0', ':)', ":(", ":D", "xD"];
+let k = 0;
+while (k < 5) {
+    console.log(p3[k]);
+    k++;
+}
+console.log("-----------")
+
+//KINTAMUJU PALYGINIMAS
+
+//1.
